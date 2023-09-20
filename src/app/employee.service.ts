025365ce4f10fe4274d9dc50fdd5016d4cd346d4
John David Dela Cruz <johndaviddc1@gmail.com>
2023-09-20 16:00:49 +0800

    added something

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Observable } from "rxjs";
 import { Employee } from "./employee";
+import { environment } from "./environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class EmployeeService {
-    private apiServerUrl = 'environment.apiBaseUrl';
+    private apiServerUrl = environment.apiBaseUrl;
 
     constructor(private http: HttpClient) { }
 
